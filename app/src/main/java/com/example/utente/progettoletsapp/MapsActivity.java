9ff1dfcb7d3bff32a6e6afa73d5bd71862b32ae0
mighelxd <mikyss38@gmail.com>
2017-05-11 22:10:16 +0200

    aggiunto bottoni e ascoltatori dei bottoni

--- app/src/main/java/com/example/utente/progettoletsapp/MapsActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/MapsActivity.java
@@ -1,8 +1,17 @@
 package com.example.utente.progettoletsapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,17 +23,51 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    DrawerLayout lmenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
+    public void buttonClicked(View v) {
+        lmenu = (DrawerLayout) findViewById(R.id.drawer_layout);
+        switch (v.getId()) {
+            //Apre il menu laterale
+            case R.id.bMenu:
+                lmenu.openDrawer(Gravity.LEFT);
+                break;
+            case R.id.bSearch:
+                Toast.makeText(this, "Premmuto ricerca", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bSave:
+                Toast.makeText(this, "Premuto salva", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+    }
+
+    public void itemClicked(View v) {
+        switch (v.getId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Cliccato primo tasto menu", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item2:
+                Toast.makeText(this, "Cliccato primo tasto menu", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item3:
+                Toast.makeText(this, "Cliccato primo tasto menu", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -40,8 +83,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
