c4431f998b3fd2fe4c7f9ceab1eae21d34fa8310
marcohl8 <marcoh_digennaro8@hotmail.com>
2017-05-12 03:08:18 +0200

    Creazione Activity SalvaLuogo

---
+++ app/src/main/java/com/example/utente/progettoletsapp/SalvaLuogoActivity.java
@@ -0,0 +1,13 @@
+package com.example.utente.progettoletsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SalvaLuogoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salva_luogo);
+    }
+}
