c1272c6fff83bac8cc8b2e34fa0a1a5cf0c09eff
marcohl8 <marcoh_digennaro8@hotmail.com>
2017-05-13 17:51:49 +0200

    Activity Visual

--- app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
@@ -2,8 +2,11 @@ package com.example.utente.progettoletsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class VisualActivity extends AppCompatActivity {
 
@@ -20,5 +23,17 @@ public class VisualActivity extends AppCompatActivity {
 
         ImageView[] stlar={(ImageView)findViewById(R.id.stl1),(ImageView) findViewById(R.id.stl2),(ImageView) findViewById(R.id.stl3),(ImageView) findViewById(R.id.stl4),(ImageView) findViewById(R.id.stl5)};
         int[] ids={R.id.stl1,R.id.stl2,R.id.stl3,R.id.stl4,R.id.stl5};
+    }
+
+        public void buttonClicked(View v){
+            if(v.getId()==R.id.bCanc){
+                Toast.makeText(VisualActivity.this,"Premuto Cancella",Toast.LENGTH_LONG).show();
+            }
+            if(v.getId()==R.id.bMod){
+                Toast.makeText(VisualActivity.this,"Premuto Modifica",Toast.LENGTH_LONG).show();
+            }
+            if(v.getId()==R.id.bPer){
+                Toast.makeText(VisualActivity.this,"Premuto Percorso",Toast.LENGTH_LONG).show();
+            }
     }
 }
