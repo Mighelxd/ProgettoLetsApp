1bdb343d8ed9ecea0a8b2766ce8ad9ebe3b4f2fd
ciro <ciroma809@gmail.com>
2017-05-13 17:21:21 +0200

    aggiunta funzione per cercare i nomi

--- app/src/main/java/com/example/utente/progettoletsapp/RicercaActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/RicercaActivity.java
@@ -1,27 +1,45 @@
 package com.example.utente.progettoletsapp;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class RicercaActivity extends AppCompatActivity {
 
+    DatabasePosti databasePosti;
+    SQLiteDatabase db;
+
     private String testoRic;
     private EditText edRic;
+
+
+    private String nomiRic[];
+    private String codiciRic[];
+    private int numVoci;
+
+    private ListView lista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ricerca);
+
         edRic=(EditText) findViewById(R.id.editText);
+
         edRic.setText(getIntent().getStringExtra("edRic"));
         TextView.OnEditorActionListener ls1=new TextView.OnEditorActionListener() {
             @Override
@@ -31,6 +49,12 @@ public class RicercaActivity extends AppCompatActivity {
             }
         };
         edRic.setOnEditorActionListener(ls1);
+
+        databasePosti=new DatabasePosti(RicercaActivity.this);
+        db=databasePosti.getReadableDatabase();
+
+        lista=(ListView) findViewById(R.id.listaPosti);
+        numVoci=0;
         bSearch();
 
     }
@@ -38,12 +62,34 @@ public class RicercaActivity extends AppCompatActivity {
         bSearch();
     }
     public void bSearch(){
-        edRic=(EditText) findViewById(R.id.editText);
+
         testoRic=edRic.getText().toString();
-        String queryRic="SELECT p.codice,p.nome FROM posto p, sottotipo s, tipo t" +
-                "WHERE p.cod_Stipo=s.codice AND s.cod_tipo=t.codice AND (p.nome LIKE '%"+testoRic+"%'" +
-                "OR p.descrizione LIKE '%"+testoRic+"%' OR s.descrizione LIKE '%"+testoRic+"%' OR t.descrizione LIKE '%"+testoRic+"%')" +
+        String queryRic="SELECT p.codice,p.nome FROM posto AS p, sottotipo AS s, tipo AS t " +
+                "WHERE p.cod_Stipo=s.codice AND s.cod_tipo=t.codice AND (p.nome LIKE '%"+testoRic+"%' " +
+                "OR p.descrizione LIKE '%"+testoRic+"%' OR s.descrizione LIKE '%"+testoRic+"%' OR t.descrizione LIKE '%"+testoRic+"%') " +
                 "ORDER BY p.Nstelle;";
+
+        Cursor cursor=db.rawQuery(queryRic,null);
+        int numVociPrec=numVoci;
+        numVoci=0;
+        if(cursor.getCount()>0) {
+            nomiRic=new String[cursor.getCount()];
+            codiciRic=new String[cursor.getCount()];
+            if(cursor.moveToFirst())
+                do {
+                    codiciRic[numVoci] = cursor.getString(0);
+                    nomiRic[numVoci++] = cursor.getString(1);
+
+                } while (cursor.moveToNext());
+
+
+            lista.setAdapter(new ArrayAdapter<String>(RicercaActivity.this,android.R.layout.simple_list_item_1,nomiRic));
+        }
+        else {
+            numVoci=0;
+            lista.setAdapter(new ArrayAdapter<String>(RicercaActivity.this,android.R.layout.simple_list_item_1,nomiRic));
+            Toast.makeText(RicercaActivity.this,"Nessun Posto trovato!",Toast.LENGTH_SHORT);
+        }
     }
 
 }
