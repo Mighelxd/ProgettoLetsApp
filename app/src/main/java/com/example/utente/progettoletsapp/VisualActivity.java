058f86ee301b321845523a45bab4ad668ebbe51d
marcohl8 <marcoh_digennaro8@hotmail.com>
2017-05-13 17:22:39 +0200

    Creazione activity informazioni

---
+++ app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
@@ -0,0 +1,13 @@
+package com.example.utente.progettoletsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visual);
+    }
+}
