575837e41d03a4c039d8089ded10a4bbde5aa36f
marcohl8 <marcoh_digennaro8@hotmail.com>
2017-05-13 17:36:30 +0200

    Activity Visual

--- app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
@@ -2,6 +2,8 @@ package com.example.utente.progettoletsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class VisualActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class VisualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visual);
+
+        final TextView txtnom=(TextView) findViewById(R.id.txtnom),
+                txttip=(TextView) findViewById(R.id.txttip),
+                txtstip=(TextView) findViewById(R.id.txtstip),
+                txtds=(TextView) findViewById(R.id.txtds),
+                txtdescr=(TextView) findViewById(R.id.txtdescr);
+
+        ImageView[] stlar={(ImageView)findViewById(R.id.stl1),(ImageView) findViewById(R.id.stl2),(ImageView) findViewById(R.id.stl3),(ImageView) findViewById(R.id.stl4),(ImageView) findViewById(R.id.stl5)};
+        int[] ids={R.id.stl1,R.id.stl2,R.id.stl3,R.id.stl4,R.id.stl5};
     }
 }
