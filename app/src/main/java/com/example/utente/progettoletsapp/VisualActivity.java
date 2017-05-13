67a89053b757e1a64bb7f2d3097c962e999f6ae2
marcohl8 <marcoh_digennaro8@hotmail.com>
2017-05-13 17:35:48 +0200

    Merge branch 'master' of C:\Users\mdcod\Desktop\Android\Android ese\ProgettoLetsApp with conflicts.

--- app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
@@ -2,7 +2,6 @@ package com.example.utente.progettoletsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class VisualActivity extends AppCompatActivity {
 
@@ -10,6 +9,5 @@ public class VisualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visual);
-        Toast.makeText(this, getIntent().getStringExtra("codice"), Toast.LENGTH_SHORT).show();
     }
 }
