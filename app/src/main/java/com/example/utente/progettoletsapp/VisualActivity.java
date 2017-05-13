db32c8a043cd5da65f039b0ccb9db5f89ec6740c
mighelxd <mikyss38@gmail.com>
2017-05-13 17:34:17 +0200

    aggiunto listener degli item

--- app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/VisualActivity.java
@@ -2,6 +2,7 @@ package com.example.utente.progettoletsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class VisualActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class VisualActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visual);
+        Toast.makeText(this, getIntent().getStringExtra("codice"), Toast.LENGTH_SHORT).show();
     }
 }
