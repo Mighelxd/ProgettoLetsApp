647a7cb81175179e204e9181363fc334157a3ad7
marcohl8 <marcoh_digennaro8@hotmail.com>
2017-05-14 19:34:06 +0200

    modifiche varie

--- app/src/main/java/com/example/utente/progettoletsapp/RicercaActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/RicercaActivity.java
@@ -141,7 +141,7 @@ public class RicercaActivity extends AppCompatActivity {
             nomiRic = new String[numVoci];
             codiciRic = new String[numVoci];
             lista.setAdapter(new ArrayAdapter<String>(RicercaActivity.this, android.R.layout.simple_list_item_1, nomiRic));
-            Toast.makeText(RicercaActivity.this, "Nessun posto ancora inserito!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(RicercaActivity.this, "Nessun posto è stato ancora inserito!", Toast.LENGTH_SHORT).show();
         }
     }
 
