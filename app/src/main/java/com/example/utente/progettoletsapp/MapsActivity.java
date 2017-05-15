6a73a27e0ca78f4906361b70587994ad335c192f
marcohl8 <marcoh_digennaro8@hotmail.com>
2017-05-15 08:50:22 +0200

    modifiche varie

--- app/src/main/java/com/example/utente/progettoletsapp/MapsActivity.java
+++ app/src/main/java/com/example/utente/progettoletsapp/MapsActivity.java
@@ -201,10 +201,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(false);
         }
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     protected synchronized void buildGoogleApiClient() {
